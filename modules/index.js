c544a074b0b7416ceb76dbf3ea4715f91973273a
mpini <mattia.pini@iit.it>
2019-02-22 09:44:50 +0100

    fix require

--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,8 @@
 const CRC32WriteStream = require('./crc32-writestream');
+const CRC32TransformStream = require('./crc32-transformstream');
+
 
 module.exports = {
     CRC32WriteStream: CRC32WriteStream,
     CRC32TransformStream: CRC32TransformStream
 };
-
